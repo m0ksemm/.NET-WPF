eb3823f222d0634b2a7aaaa98ccd29230a5fe513
m0ksemm <md.cheban@gmail.com>
2023-11-18 11:22:49 +0200

    WPF introduction

---
+++ 1. Введение в WPF/1. Введение в WPF/Introduction to WPF/Setting properties/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Setting_properties
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LinearGradientBrush brush = new LinearGradientBrush();
+            GradientStop gradientStop1 = new GradientStop();
+            gradientStop1.Offset = 0;
+            gradientStop1.Color = Colors.Red;
+            brush.GradientStops.Add(gradientStop1);
+            GradientStop gradientStop2 = new GradientStop();
+            gradientStop2.Offset = 0.5;
+            gradientStop2.Color = Colors.Indigo;
+            brush.GradientStops.Add(gradientStop2);
+            GradientStop gradientStop3 = new GradientStop();
+            gradientStop3.Offset = 1;
+            gradientStop3.Color = Colors.Violet;
+            brush.GradientStops.Add(gradientStop3);
+            grid1.Background = brush;
+        }
+
+        private void cmdAnswer_Click(object sender, RoutedEventArgs e)
+        {
+            txt2.Text = txt1.Text;
+        }
+    }
+}
