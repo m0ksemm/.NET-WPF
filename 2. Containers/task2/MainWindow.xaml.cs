d26bab2d417abc5b78eb64389a91bd0fa872c909
m0ksemm <md.cheban@gmail.com>
2023-11-18 11:25:19 +0200

    Containers

---
+++ 2. Containers/task2/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace task2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int password;
+        public MainWindow()
+        {
+            InitializeComponent();
+            GeneratePassword();
+        }
+
+        private void GeneratePassword() 
+        {
+            Random rnd = new Random();
+            password = rnd.Next(10000, 99999);
+            Title = "Password: " + password.ToString();
+        }
+        private void Check() 
+        {
+            if (txt1.Text != "")
+            {
+                int n = int.Parse(txt1.Text);
+                if (n == password)
+                {
+                    MessageBoxResult result = MessageBox.Show("Password is correct!\nNew password was generated", "Confirmation");
+                    GeneratePassword();
+                    txt1.Clear();
+                }
+                else
+                {
+                    MessageBoxResult result = MessageBox.Show("Wrong Password!\nTry again", "Not Confirmed");
+                    txt1.Clear();
+                }
+            }
+        }
+        private void cmdAnswer_Click(object sender, RoutedEventArgs e)
+        {
+            if (e.Source == cmdAnswer7) 
+            {
+                txt1.Text += "7";
+            }
+            else if (e.Source == cmdAnswer8)
+            {
+                txt1.Text += "8";
+            }
+            if (e.Source == cmdAnswer9)
+            {
+                txt1.Text += "9";
+            }
+            if (e.Source == cmdAnswer4)
+            {
+                txt1.Text += "4";
+            }
+            if (e.Source == cmdAnswer5)
+            {
+                txt1.Text += "5";
+            }
+            if (e.Source == cmdAnswer6)
+            {
+                txt1.Text += "6";
+            }
+            if (e.Source == cmdAnswer3)
+            {
+                txt1.Text += "3";
+            }
+            if (e.Source == cmdAnswer2)
+            {
+                txt1.Text += "2";
+            }
+            if (e.Source == cmdAnswer1)
+            {
+                txt1.Text += "1";
+            }
+            if (e.Source == cmdAnswer0)
+            {
+                txt1.Text += "0";
+            }
+            if (e.Source == cmdAnswerC)
+            {
+                txt1.Clear();
+            }
+            if (e.Source == cmdAnswerOk) 
+            {
+                Check();
+            }
+        }
+    }
+    
+}
---
+++ 2. Containers/task3/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace task3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void cmd_result_Click(object sender, RoutedEventArgs e)
+        {
+            double[,] matr = new double[3,3];
+
+            matr[0, 0] = double.Parse(txt1.Text);
+            matr[0, 1] = double.Parse(txt2.Text);
+            matr[0, 2] = double.Parse(txt3.Text);
+
+            matr[1, 0] = double.Parse(txt4.Text);
+            matr[1, 1] = double.Parse(txt5.Text);
+            matr[1, 2] = double.Parse(txt6.Text);
+
+            matr[2, 0] = double.Parse(txt7.Text);
+            matr[2, 1] = double.Parse(txt8.Text);
+            matr[2, 2] = double.Parse(txt9.Text);
+
+            res.Text = Determinant(matr).ToString();
+        }
+        double Determinant(double[,] matr)  //нахождение определителя по минорам квадратичной матрицы
+        {
+            return matr[0,0] * matr[1,1] * matr[2,2] - matr[0,0] * matr[1,2] * matr[2,1]
+                 - matr[0,1] * matr[1,0] * matr[2,2] + matr[0,1] * matr[1,2] * matr[2,0]
+                 + matr[0,2] * matr[1,0] * matr[2,1] - matr[0,2] * matr[1,1] * matr[2,0];
+        }
+        //PreviewTextInput="pnl_PreviewTextInput" PreviewKeyDown="pnl_PreviewKeyDown"
+        private void pnl_PreviewTextInput(object sender, TextCompositionEventArgs e) 
+        {
+            short val;
+            if (!Int16.TryParse(e.Text, out val)) 
+            {
+                e.Handled = true;
+            }
+        }
+        private void pnl_PreviewKeyDown(object sender, KeyEventArgs e) 
+        {
+            if (e.Key == Key.Space) 
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
