9ecbc6cbe591e135a910d96916c48f326a536823
m0ksem <md.cheban@gmail.com>
2025-08-06 22:26:05 +0300

    Updated the Design of new Calculator.

--- 3. Calculator/3. Calculator/obj/Debug/net6.0-windows/App.g.i.cs
+++ 3. Calculator/3. Calculator/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace _3._Calculator {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.7.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace _3._Calculator {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.7.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
         public static void Main() {
             _3._Calculator.App app = new _3._Calculator.App();
             app.InitializeComponent();
