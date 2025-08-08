7165bb33d383c2af9343a27abba22620da6f357a
m0ksem <md.cheban@gmail.com>
2025-08-08 21:19:55 +0300

    Completed the main functional of Contact app. Will add some design updates a bit later

--- DesktopContactsApp/DesktopContactsApp/NewContactWindow.xaml.cs
+++ DesktopContactsApp/DesktopContactsApp/NewContactWindow.xaml.cs
@@ -24,6 +24,9 @@ namespace DesktopContactsApp
         public NewContactWindow()
         {
             InitializeComponent();
+
+            Owner = Application.Current.MainWindow;
+            WindowStartupLocation = WindowStartupLocation.CenterOwner;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
